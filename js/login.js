5d25d52ca3c3bbc8a8548db32c620f6ad08ae867
Md Jeshan Khan <146430693+jeshanTheKhan@users.noreply.github.com>
2025-03-25 15:36:22 +0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.getElementById("login-btn").addEventListener("click",function(event){
   if(accountNumber.length === 11 && pinNumber.length === 4)
     if(pinNumber == 1234){
     // console.log(accountNumber,pinNumber);
-    window.location.href="pages/main.html"
+    window.location.href="../pages/main.html"
   }
   else{
     alert("Please Enter valid password")
